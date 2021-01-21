747993eadac0bc2692e6fa27033d1e6a07054272
Kainoa Brabo <kaibrabo@gmail.com>
2021-01-21 14:01:09 -0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDlhP5HOAHkwtidsIc8aNxgVh7r1WfgzrA",
+    authDomain: "diwallet-c3bfb.firebaseapp.com",
+    projectId: "diwallet-c3bfb",
+    storageBucket: "diwallet-c3bfb.appspot.com",
+    messagingSenderId: "754128128593",
+    appId: "1:754128128593:web:cab3636f980ecffb894670"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
